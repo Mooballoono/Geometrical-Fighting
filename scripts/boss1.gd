72ed6698c77a23065c8af867ce5e0f3e4ccc123c
Mooballoono <masinkometz@gmail.com>
2024-07-01 01:20:22 -0700

    Fixed the terrible health bar system

--- scripts/boss1.gd
+++ scripts/boss1.gd
@@ -6,18 +6,8 @@ const max_health: int = 10
 var health: float = 10
 var health_percent: float = 1
 
-var files = []
-var dir = DirAccess.open("E://Programs//Godot//Geometrical Fighting//graphics//health_bar")
-
 func _ready():
-	dir.list_dir_begin()
-	while true:
-		var file = dir.get_next()
-		if file == "":
-			break
-		elif file.begins_with("health_bar") and not file.ends_with(".import"):
-			files.append("E://Programs//Godot//Geometrical Fighting//graphics//health_bar//" + file)
-	files.reverse()
+	pass
 
 func _process(_delta):
 	pass
@@ -25,8 +15,7 @@ func _process(_delta):
 func hit(damage):
 	health -= damage
 	health_percent = float(health/max_health)
-	var health_bar_index: int = ceil(health_percent*len(files))
-	$HealthBarSprite2D.texture = load(files[health_bar_index])
+	$TextureProgressBar.set_value(ceil(health_percent*100))
 	if health <= 0:
 		dead.emit()
 		queue_free()
